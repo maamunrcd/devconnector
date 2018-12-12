45df4e850561a2dd0305d18c98d8b505b169beb4
Mamun OR Rashid <mamun.rashid@bs-23.net>
2018-12-12 12:42:37 +0600

    Create profile education and experience post private route with validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,8 @@ const passport = require("passport");
 //Load validation
 
 const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 //Load Profile Model
 const Profile = require("../../models/Profile");
@@ -174,23 +176,68 @@ router.post(
 // @desc Add experience to profile
 // @access Private
 
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
 
-router.post('/experience', passport.authenticate('jwt', {session:false}), (req, res)=>{
-  Profile.findOne({user: req.user.id})
-  .then(profile => {
-    const newExp = {
-      title: req.body.title,
-      company: req.body.company,
-      location: req.body.location,
-      form: req.body.form,
-      to: req.body.to,
-      current: req.body.current,
-      description: req.body.description
+    //Check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
     }
 
-    // Add to exp array 
-    profile.experience.unshift()  
-  })
-})
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to exp array
+      profile.experience.unshift(newExp);
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+// @route POST api/profile/education
+// @desc Add education to profile
+// @access Private
+
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      //Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to exp array
+      profile.education.unshift(newEdu);
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
 
 module.exports = router;
