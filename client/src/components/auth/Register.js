301a3170a9f5dcd7214977df739d380dd4e00176
Mamun OR Rashid <mamun.rashid@bs-23.net>
2018-12-19 21:30:16 +0600

    Implement reduser and create auth using redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import classnames from "classnames";
+import { connect } from "react-redux";
+import { registeruser } from "../../actions/authAction";
 
 class Register extends Component {
   constructor() {
@@ -28,15 +30,20 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    axios
-      .post("/api/users/register", newUser)
-      .then(res => console.log(res))
-      .catch(err => this.setState({ errors: err.response.data }));
+
+    this.props.registeruser(newUser);
+
+    // axios
+    //   .post("/api/users/register", newUser)
+    //   .then(res => console.log(res))
+    //   .catch(err => this.setState({ errors: err.response.data }));
   }
   render() {
     const { errors } = this.state;
+    const { user } = this.props.auth;
     return (
       <div className="register">
+        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -119,4 +126,11 @@ class Register extends Component {
   }
 }
 
-export default Register;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { registeruser }
+)(Register);
