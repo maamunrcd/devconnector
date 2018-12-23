62cffef975c354d3bf94bf9a81ed9d4630ec0218
maamunrcd <maamunrcd@gmail.com>
2018-12-24 00:41:33 +0600

    User ragistration error handleing using Redux and Compleate the registration and rederect the login page using React-router-dom history push

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
 import classnames from "classnames";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { registeruser } from "../../actions/authAction";
 
@@ -18,6 +18,13 @@ class Register extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
@@ -32,16 +39,12 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registeruser(newUser);
-
-    // axios
-    //   .post("/api/users/register", newUser)
-    //   .then(res => console.log(res))
-    //   .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registeruser(newUser, this.props.history);
   }
   render() {
     const { errors } = this.state;
     const { user } = this.props.auth;
+    console.log(this.props.errors);
     return (
       <div className="register">
         {user ? user.name : null}
@@ -126,16 +129,18 @@ class Register extends Component {
     );
   }
 }
-Register.PropTypes = {
+Register.propTypes = {
   registeruser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
   { registeruser }
-)(Register);
+)(withRouter(Register));
